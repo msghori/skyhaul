88be1244f62fdea3e018e0b848d0ac4e1b22e518
Shoaib <msghori.dev@gmail.com>
2026-08-25 23:35:26 +0500

    fix modifications

--- vite.config.js
+++ vite.config.js
@@ -1,12 +1,12 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
-import tailwindcss from '@tailwindcss/vite'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+import tailwindcss from "@tailwindcss/vite";
 
 export default defineConfig({
-  base: '/skyhaul/',
+  base: "/",
   plugins: [react(), tailwindcss()],
   server: {
     host: true,
     port: 5173,
   },
-})
+});
